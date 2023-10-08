32d386233da927a33da1c855c8412ffa0ce33dc0
Gleb Kulik <reeeade@gmail.com>
2023-10-08 15:55:20 +0300

    Новый код

--- hw_3.2.py
+++
@@ -1,6 +0,0 @@
-first_class = int(input("Учеников в первом классе: "))
-second_class = int(input("Учеников во втором классе: "))
-third_class = int(input("Учеников в третьем классе: "))
-all_pupils = first_class + second_class + third_class
-need = (all_pupils + 1) // 2
-print("На", all_pupils, "учеников/ученика необходимо закупить парт:", need, "шт.")
