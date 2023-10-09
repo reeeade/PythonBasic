74cc72c69bc56d92b5eb4eb9dd9266f56b641d22
Gleb Kulik <reeeade@gmail.com>
2023-10-09 11:16:23 +0300

    Задание 3.2

--- HW3/hw_3.2.py
+++ HW3/hw_3.2.py
@@ -1,6 +1,5 @@
 first_class = int(input("Учеников в первом классе: "))
 second_class = int(input("Учеников во втором классе: "))
 third_class = int(input("Учеников в третьем классе: "))
-all_pupils = first_class + second_class + third_class
-need = (all_pupils + 1) // 2
-print("На", all_pupils, "учеников/ученика необходимо закупить парт:", need, "шт.")
+need = (first_class + 1) // 2 + (second_class + 1) // 2 + (third_class + 1) // 2
+print("Необходимо закупить парт:", need, "шт.")
