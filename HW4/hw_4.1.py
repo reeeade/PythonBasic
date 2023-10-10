99b985321006d08005cd2185993e7f5a2919fcc0
Gleb Kulik <reeeade@gmail.com>
2023-10-10 14:52:58 +0300

    Homework №4

--- HW4/hw_4.1.py
+++ HW4/hw_4.1.py
@@ -6,13 +6,10 @@ else:
     if len(inp_string) < 15:
         koef = 15 // len(inp_string) + 1
         inp_string *= koef
-
     new_list = list(inp_string)
     print("Полный список:", new_list)
     print("Последние 5 элементов:", new_list[-5:])
     print("Зеркальный список:", new_list[::-1])
     print("Все парные элементы:", new_list[::2])
-
     custom_list = new_list[:-5] + new_list[:5]
     print("5 элементов вначале = 5 элементам в конце списка:", custom_list)
-
