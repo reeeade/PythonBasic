351daf576f0fb9c5e29b464260157734aa533d12
Gleb Kulik <reeeade@gmail.com>
2023-10-13 19:43:59 +0300

    mod hm_5.4

--- HW5/hw_5.4.py
+++ HW5/hw_5.4.py
@@ -1,6 +1,6 @@
 num = int(input("Введите число: "))
 i = 0
-print(num, '--квадраты--->', end=" ")
+print(num, '>--квадраты--->', end=" ")
 while i ** 2 <= num:
     print(i ** 2, end=" ")
     i += 1
