e9d197cc93040726809653922bf1366e91589837
Gleb Kulik <reeeade@gmail.com>
2023-10-25 17:02:10 +0300

    Mod hw_9.1, add hw_9.2

--- HW9/hw_9.1.py
+++ HW9/hw_9.1.py
@@ -1,14 +1,14 @@
 from random import randint
 
 int_elements = [randint(1, 500) for _ in range(25)]
-print(int_elements)
+print("Список int_elements:", int_elements)
 sqr_elements = [i ** 2 for i in int_elements]
-print(sqr_elements)
+print("Список з квадратів int_elements:", sqr_elements)
 remnant_elements = [i % 11 for i in int_elements]
-print(remnant_elements)
+print("Список з остач ділення на 11 елементів int_elements:", remnant_elements)
 even_elements = [i for i in int_elements if i % 2 == 0]
-print(even_elements)
+print("Список з парних елементів int_elements:", even_elements)
 odd_len_elements = [i for i in int_elements if len(str(i)) % 2 != 0]
-print(odd_len_elements)
+print("Список з непарною кількістю цифр:", odd_len_elements)
 no_three_elements = [int_elements[i] for i in range(len(int_elements)) if i % 3 != 0]
-print(no_three_elements)
+print("Список з елементів, що стоять на позиціях які не є кратними 3:", no_three_elements)
---
+++ HW9/hw_9.2.py
@@ -0,0 +1,11 @@
+from random import randint
+
+matrix_size = int(input("Введите размер квадратной матрицы: "))
+matrix = [[randint(1, 9) for _ in range(matrix_size)] for __ in range(matrix_size)]
+print("Исходная матрица:")
+for i in matrix:
+    print(i)
+d_sum = sum(matrix[i][i] for i in range(matrix_size))
+print("Сумма чисел по диагонали:", d_sum)
+last_sum = sum(matrix[i][matrix_size - 1] for i in range(matrix_size))
+print("Сумма чисел последнего столбика:", last_sum)
